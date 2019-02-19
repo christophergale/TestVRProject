653bfaaa0f90fd79ce7198e9675077c337341a2f
Christopher Gale <cdgale@gmail.com>
2019-02-19 14:36:26 +0000

    Clone changes
    Added an editable maximum number of clones. Clones are now added to an array and will be deleted in order when the maximum clone count is reached.

--- Assets/Scripts/Clone.cs
+++ Assets/Scripts/Clone.cs
@@ -5,22 +5,42 @@ using UnityEngine;
 public class Clone : CliveClass {
 
     GameObject clonePiece;
+    public GameObject[] clones;
+
+    public int cloneMaximum = 3;
+    private int cloneCurrent = 0;
 
 	// Use this for initialization
 	void Start () {
         clonePiece = GetComponent<Clive>().cliveCopy;
+        clones = new GameObject[cloneMaximum];
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.G))
         {
+            Debug.Log("G key pressed");
             CloneClive();
         }
 	}
 
     void CloneClive()
     {
-        Instantiate(clonePiece, this.transform.position, this.transform.rotation);
+        if (cloneCurrent < cloneMaximum)
+        {
+            if (clones[cloneCurrent] != null)
+            {
+                Destroy(clones[cloneCurrent]);
+            }
+
+            clones[cloneCurrent] = Instantiate(clonePiece, this.transform.position, this.transform.rotation);
+            cloneCurrent += 1;
+        }
+        else
+        {
+            cloneCurrent = 0;
+        }
+        
     }
 }
