c4f9477c238f9f38d4d5c80a5f98098274a27c53
Christopher Gale <cdgale@gmail.com>
2019-03-08 16:43:56 +0000

    Added TetrisChanger

---
+++ Assets/Scripts/TetrisChanger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisChanger : ActivatableTarget {
+
+    public bool isTriggerChanger;
+
+    public Tetris.TetrisShape changeTetrisShape;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Clive") && isTriggerChanger)
+        {
+            ExecuteOnActivate();
+        }
+    }
+
+    public override void ExecuteOnActivate()
+    {
+        base.ExecuteOnActivate();
+
+        if (Clive.instance.GetComponent<Tetris>() && Clive.instance.GetComponent<Tetris>().tetrisShape != changeTetrisShape)
+        {
+            Clive.instance.GetComponent<Tetris>().tetrisShape = changeTetrisShape;
+        }
+    }
+}
