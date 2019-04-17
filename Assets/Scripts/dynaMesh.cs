14893e29a5e5b2ef344ce54a498f0a06cba10b07
QIZHI GUO <1705304@uad.ac.uk>
2019-04-17 16:23:46 +0100

    Character Colliders Updated
    Update 2 solutions of character colliders and set tags ready for interaction.

---
+++ Assets/Scripts/dynaMesh.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dynaMesh : MonoBehaviour
+{
+    [Tooltip("Put the skinned mesh renderer of this game object here.")]
+    public SkinnedMeshRenderer meshRenderer;
+    [Tooltip("Create an empty mesh collider of this game object and put it here.")]
+    public MeshCollider meshCollider;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Mesh colliderMesh = new Mesh(); // Memory leak
+        meshRenderer.BakeMesh(colliderMesh);
+        meshCollider.sharedMesh = colliderMesh;
+    }
+}
