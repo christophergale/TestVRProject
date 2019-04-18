41e60c31a327b386ca7ceb39ff6056b5948bf1f1
QIZHI GUO <1705304@uad.ac.uk>
2019-04-18 14:59:04 +0100

    Small Adjustment on Character
    Add more details to the interactive character and add the sample script of player detector.

--- Assets/Scripts/PlayerDetector.cs
+++ Assets/Scripts/PlayerDetector.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class PlayerDetector : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Tooltip("If this component will reflect on persistent interaction.")]
+    public bool isImmediate;
+    [Tooltip("If this component will reflect on persistent interaction.")]
+    public bool isPersistent;
+
+    int colliderNum = 0;
+
+    private void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            colliderNum++;
+
+            if ((colliderNum == 0) && isImmediate)
+            {
+                // Do something immediately as the collision happens.
+                Debug.Log("Player detected!");
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") && isPersistent)
+        {
+            // Do something while the trigger is being touched continuously.
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            colliderNum--;
+        }
+    }
 }
