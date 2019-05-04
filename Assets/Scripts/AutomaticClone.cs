ce1e011f14c5d3a766e40d3685e8e38ec7fd60d0
GozWit <medlar_guo@outlook.com>
2019-05-04 17:44:44 +0100

    Testing Adjustment

--- Assets/Scripts/AutomaticClone.cs
+++ Assets/Scripts/AutomaticClone.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AutomaticClone : MonoBehaviour {
 
+    public float waitingTime = 5f;
+
     bool checking = false;
 
     // Use this for initialization
@@ -28,7 +30,7 @@ public class AutomaticClone : MonoBehaviour {
 
         float tolerance = 0.3f;
 
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(waitingTime);
 
         if (position.sqrMagnitude >= tolerance * tolerance)
         {
